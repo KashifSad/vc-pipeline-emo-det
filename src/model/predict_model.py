4f81ff606958030726e2976e827194cf14cc2aeb
KashifSad <kashraza010@gmail.com>
2024-07-09 12:23:47 +0500

    add exp tracking code

--- src/model/predict_model.py
+++ src/model/predict_model.py
@@ -3,6 +3,8 @@ import numpy as np
 import pickle
 from sklearn.metrics import accuracy_score,precision_score,recall_score,roc_auc_score,classification_report
 import json
+from dvclive import Live
+import yaml
 
 test_df = pd.read_csv('./data/processed/test_bow.csv')
 
@@ -18,6 +20,21 @@ accuracy = accuracy_score(y_test,y_pred)
 precision = precision_score(y_test,y_pred)
 recall = recall_score(y_test,y_pred)
 auc = roc_auc_score(y_test,y_pred)
+
+
+with open('params.yaml','r') as file:
+    params = yaml.safe_load(file)
+
+
+with Live(save_dvc_exp=True) as live:
+    live.log_metric('accuracy',accuracy)
+    live.log_metric('precision',precision)
+    live.log_metric('recall',recall)
+    live.log_metric('auc',auc)
+
+    for param,value in params.items():
+        for key,val in value.items():
+            live.log_param(f'{param}_{key}',val)
 
 
 metrics_dict = {
